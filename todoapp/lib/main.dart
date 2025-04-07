0bf7a486e827f5f2de01ce51529447e79af5c889
Isaac Lins <contact@isaaclins.com>
2025-04-07 12:36:49 +0200

    Update .gitignore to include Flutter/Dart/Pub related directories for better project management

---
+++ todoapp/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const CupertinoApp(
+      home: CupertinoPageScaffold(
+        navigationBar: CupertinoNavigationBar(middle: Text('Todo App')),
+        child: Center(child: Text('Hello World!')),
+      ),
+    );
+  }
+}
